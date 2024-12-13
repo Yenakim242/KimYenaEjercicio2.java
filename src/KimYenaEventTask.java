e4a8fe901ec598d791053f758286eb14aff1043b
YENA <130546842+Yenakim242@users.noreply.github.com>
2024-12-14 00:10:36 +0100

    EventTask Updated

--- src/KimYenaEventTask.java
+++ src/KimYenaEventTask.java
@@ -1,2 +1,22 @@
-public class KimYenaEventTask {
+public class KimYenaEventTask{
+    // Descripción de la tarea
+    private String text;
+    // Estado de la tarea
+    private boolean isCompleted;
+
+    // Constructor de la clase EventTask
+    public KimYenaEventTask(String text){
+        this.text = text;
+        this.isCompleted = false;
+    }
+
+    // Marca o desmarca la tarea como completada.
+    public void toggleCompletion() {
+        isCompleted = !isCompleted;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Tarea: %s [Estado: %s]", text, isCompleted ? "Completada" : "Pendiente");
+    }
 }
